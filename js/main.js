339abfd301797bca8284699f5feb7533538876fa
Maxim <54853308+SaintShamon@users.noreply.github.com>
2020-08-10 15:56:12 +0300

    Файлы для сайта

---
+++ js/main.js
@@ -0,0 +1,21 @@
+$('.advantages-slider').slick( {
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 3000,
+        accessibility: true,
+        arrows: true,
+    }
+
+);
+
+$('.partners-slider').slick( {
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 3000,
+        accessibility: true,
+        arrows: true,
+    }
+
+);
