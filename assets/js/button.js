f0df32f80f221fffb592848261ebe5c083412828
SaintShamon <54853308+SaintShamon@users.noreply.github.com>
2020-08-26 23:21:20 +0300

    js

--- assets/js/button.js
+++ assets/js/button.js
@@ -31,12 +31,46 @@ $(document).ready(function () {
     });
 });
 
-document.getElementById('1').onclick = f1;
+function btn_1() {
+    let name = document.querySelector('#name_1');
+    let percent = document.querySelector('#percent_1');
+    let money = document.querySelector('#money_1');
 
-function f1() {
-    let name = document.getElementById('name');
-    let percent = document.getElementById('percent');
-    let money = document.getElementById('money');
-
-    document.querySelector('.drawer-menu-text') = name.textContent;
+    document.querySelector('.drawer-menu-text').textContent = name.textContent;
+    document.querySelector('.percent').textContent = percent.textContent;
+    document.querySelector('.money').textContent = money.textContent;
 }
+document.querySelector('#btn_1').onclick = btn_1;
+
+function btn_2() {
+    let name = document.querySelector('#name_2');
+    let percent = document.querySelector('#percent_2');
+    let money = document.querySelector('#money_2');
+
+    document.querySelector('.drawer-menu-text').textContent = name.textContent;
+    document.querySelector('.percent').textContent = percent.textContent;
+    document.querySelector('.money').textContent = money.textContent;
+}
+document.querySelector('#btn_2').onclick = btn_2;
+
+function btn_3() {
+    let name = document.querySelector('#name_3');
+    let percent = document.querySelector('#percent_3');
+    let money = document.querySelector('#money_3');
+
+    document.querySelector('.drawer-menu-text').textContent = name.textContent;
+    document.querySelector('.percent').textContent = percent.textContent;
+    document.querySelector('.money').textContent = money.textContent;
+}
+document.querySelector('#btn_3').onclick = btn_3;
+
+function btn_4() {
+    let name = document.querySelector('#name_4');
+    let percent = document.querySelector('#percent_4');
+    let money = document.querySelector('#money_4');
+
+    document.querySelector('.drawer-menu-text').textContent = name.textContent;
+    document.querySelector('.percent').textContent = percent.textContent;
+    document.querySelector('.money').textContent = money.textContent;
+}
+document.querySelector('#btn_4').onclick = btn_4;
