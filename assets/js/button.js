4088f0d38a830cd794d65f7731af200cd6d1e268
SaintShamon <54853308+SaintShamon@users.noreply.github.com>
2020-08-26 22:19:45 +0300

    +

---
+++ assets/js/button.js
@@ -0,0 +1,42 @@
+/*A jQuery plugin which add loading indicators into buttons*/
+
+(function ($) {
+    $.fn.buttonLoader = function (action) {
+        var self = $(this);
+        if (action == 'start') {
+            if ($(self).attr("disabled") == "disabled") {
+                e.preventDefault();
+            }
+            $('.has-spinner').attr("disabled", "disabled");
+            $(self).attr('data-btn-text', $(self).text());
+            $(self).html('<span class="spinner"><i class="fa fa-spinner fa-spin"></i></span>Подключение');
+            $(self).addClass('active');
+        }
+        if (action == 'stop') {
+            $(self).html($(self).attr('data-btn-text'));
+            $(self).removeClass('active');
+            $('.has-spinner').removeAttr("disabled");
+        }
+    }
+})(jQuery);
+
+$(document).ready(function () {
+
+    $('.has-spinner').click(function () {
+        var btn = $(this);
+        $(btn).buttonLoader('start');
+        setTimeout(function () {
+            $(btn).buttonLoader('stop');
+        }, 3000);
+    });
+});
+
+document.getElementById('1').onclick = f1;
+
+function f1() {
+    let name = document.getElementById('name');
+    let percent = document.getElementById('percent');
+    let money = document.getElementById('money');
+
+    document.querySelector('.drawer-menu-text') = name.textContent;
+}
