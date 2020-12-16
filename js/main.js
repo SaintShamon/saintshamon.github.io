c6ad135012758315bc1c1b1f2491daa1e5393200
Maxim <54853308+SaintShamon@users.noreply.github.com>
2020-12-16 04:32:57 +0200

    +

--- js/main.js
+++ js/main.js
@@ -1,27 +1,66 @@
+$(function () {
+    var Accordion = function (el, multiple) {
+        this.el = el || {};
+        this.multiple = multiple || false;
+
+        var links = this.el.find('.article-title');
+        links.on('click', {
+            el: this.el,
+            multiple: this.multiple
+        }, this.dropdown)
+    }
+
+    Accordion.prototype.dropdown = function (e) {
+        var $el = e.data.el;
+        $this = $(this),
+            $next = $this.next();
+
+        $next.slideToggle();
+        $this.parent().toggleClass('open');
+
+        if (!e.data.multiple) {
+            $el.find('.accordion-content').not($next).slideUp().parent().removeClass('open');
+        };
+    }
+    var accordion = new Accordion($('.accordion-container'), false);
+});
+
+$(document).on('click', function (event) {
+    if (!$(event.target).closest('#accordion').length) {
+        $this.parent().toggleClass('open');
+    }
+});
+
+// slider
+
+$('.review_block').slick({
+    dots: true,
+    arrows: false,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 1,
+    adaptiveHeight: true
+});
+
+
+// popup
+
 $(document).ready(function () {
-    $(".set > a").on("click", function () {
-        if ($(this).hasClass("active")) {
-            $(this).removeClass("active");
-            $(this)
-                .siblings(".content")
-                .slideUp(200);
-            $(".set > a i")
-                .removeClass("fa-minus")
-                .addClass("fa-plus");
-        } else {
-            $(".set > a i")
-                .removeClass("fa-minus")
-                .addClass("fa-plus");
-            $(this)
-                .find("i")
-                .removeClass("fa-plus")
-                .addClass("fa-minus");
-            $(".set > a").removeClass("active");
-            $(this).addClass("active");
-            $(".content").slideUp(200);
-            $(this)
-                .siblings(".content")
-                .slideDown(200);
+    $('.popup-with-form').magnificPopup({
+        type: 'inline',
+        preloader: false,
+        focus: '#name',
+
+        // When elemened is focused, some mobile browsers in some cases zoom in
+        // It looks not nice, so we disable it:
+        callbacks: {
+            beforeOpen: function () {
+                if ($(window).width() < 700) {
+                    this.st.focus = false;
+                } else {
+                    this.st.focus = '#name';
+                }
+            }
         }
     });
 });
