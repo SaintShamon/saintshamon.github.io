ddfa949b929166f863688eddaff9cf85b49851e8
Maxim <54853308+SaintShamon@users.noreply.github.com>
2021-02-01 21:34:16 +0200

    +

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
-$('.video1 .video1-play').click(function () {
+$('.video1-play').click(function () {
     video = '<iframe src="' + $(this).attr('data-video') + '"  frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>';
     $(this).replaceWith(video);
 });
 
-$('.video2 .video2-play').click(function () {
+$(' .video2-play').click(function () {
     video = '<iframe src="' + $(this).attr('data-video') + '"  frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>';
     $(this).replaceWith(video);
 });
