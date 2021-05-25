a51b9349c22d2d9c2b3c15c3a2a39b4f64ae0e54
Maxim <54853308+SaintShamon@users.noreply.github.com>
2021-05-25 20:41:36 +0300

    +

--- js/main.js
+++ js/main.js
@@ -1,93 +1,47 @@
-$(document).ready(function () {
-    $('.review_container').slick({
-        infinite: true,
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        dots: false,
-        prevArrow: $(".pp2"),
-        nextArrow: $(".nn2"),
-    });
+var btn = $('.btn');
 
-    $('.partners_block').slick({
-        slidesToShow: 4,
-        slidesToScroll: 1,
-        dots: true,
-        infinite: false,
-        arrows: false,
-        responsive: [
-            {
-                breakpoint: 1024,
-                settings: {
-                    slidesToShow: 3,
-                    slidesToScroll: 1,
-                    infinite: true,
-                    dots: true
-                }
-            },
-            {
-                breakpoint: 600,
-                settings: {
-                    slidesToShow: 2,
-                    slidesToScroll: 1
-                }
-            },
-            {
-                breakpoint: 480,
-                settings: {
-                    slidesToShow: 1,
-                    slidesToScroll: 1
-                }
-            }
-            // You can unslick at a given breakpoint now by adding:
-            // settings: "unslick"
-            // instead of a settings object
-        ]
-    });
+btn.on('click', function () {
+    $(this).toggleClass('active');
+    $(this).toggleClass('not-active');
+});
 
-    $('.review_block_about').slick({
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        dots: true,
-        infinite: false,
-        arrows: false,
+var btn2 = $('.show');
+
+btn2.on('click', function () {
+    $(this).toggleClass('background');
+});
+
+$(function () {
+    $('.show').on('click', function () {
+        $('.navigation').hasClass('open') ? $('.navigation').removeClass('open') : $('.navigation').addClass('open');
+        $('.toggle').next().slideUp(350);
     });
 });
 
-(function () {
-    var burger = document.querySelector('.burger-container'),
-        header = document.querySelector('.header');
 
-    burger.onclick = function () {
-        header.classList.toggle('menu-opened');
+$('.toggle').click(function (e) {
+    e.preventDefault();
+
+    var $this = $(this);
+
+    if ($this.next().hasClass('show')) {
+        $this.next().removeClass('show');
+        $this.next().slideUp(350);
+    } else {
+        $this.next().toggleClass('show');
+        $this.next().slideToggle(350);
     }
-}
-
-    ());
-
-// let list = document.querySelectorAll(".menu > .menu-item > a");
-// list.forEach(i => {
-//     i.addEventListener('click', function (e) {
-//         e.preventDefault();
-//         document.querySelector('.header.menu-opened').classList.remove("menu-opened");
-
-
-//     })
-// });
-
-let btn = document.querySelectorAll(".header .menu_btn");
-btn.forEach(i => {
-    i.addEventListener('click', function (e) {
-        e.preventDefault();
-        document.querySelector('.header.menu-opened').classList.remove("menu-opened");
-
-
-    })
 });
 
+
+// popup
+
 $(document).ready(function () {
-    $('.popup-with-form').magnificPopup({
+    $('.popup-with-zoom-anim').magnificPopup({
         type: 'inline',
         preloader: false,
+        removalDelay: 300,
+        mainClass: 'my-mfp-zoom-in',
         focus: '#name',
 
         // When elemened is focused, some mobile browsers in some cases zoom in
@@ -102,25 +56,4 @@ $(document).ready(function () {
             }
         }
     });
-
-    $(document).on('click', '.popup-modal-dismiss', function (e) {
-        e.preventDefault();
-        $.magnificPopup.close();
-    });
 });
-
-function _class(name) {
-    return document.getElementsByClassName(name);
-}
-
-let tabPanes = _class("tab-header")[0].getElementsByTagName("div");
-
-for (let i = 0; i < tabPanes.length; i++) {
-    tabPanes[i].addEventListener("click", function () {
-        _class("tab-header")[0].getElementsByClassName("active")[0].classList.remove("active");
-        tabPanes[i].classList.add("active");
-        _class("tab-content")[0].getElementsByClassName("active")[0].classList.remove("active");
-        _class("tab-content")[0].getElementsByClassName("tab-content_block")[i].classList.add("active");
-
-    });
-}
