1dcc3b9a5b36de44c8acc391f6aecaa6f13480ea
Maxim <54853308+SaintShamon@users.noreply.github.com>
2021-07-15 02:36:47 +0300

    +

--- js/main.js
+++ js/main.js
@@ -7,7 +7,6 @@ Modernizr.on('webp', function (result) {
     }
 });
 
-
 // slick slider
 
 $(document).ready(function () {
@@ -193,4 +192,33 @@ jQuery(document).ready(function ($) {
 $('.blockMenu .burgerButton').on('click', function () {
     $(this).toggleClass('active');
     $('.blockMenu .menu').toggleClass('active');
+});
+
+$(document).ready(function () {
+
+    var $animation_elements = $('.animate');
+    var $window = $(window);
+
+    function check_if_in_view() {
+        var window_height = $window.height();
+        var window_top_position = $window.scrollTop() - 200;
+        var window_bottom_position = (window_top_position + window_height);
+
+
+
+        $.each($animation_elements, function () {
+            var $element = $(this);
+            var element_height = $element.outerHeight();
+            var element_top_position = $element.offset().top;
+            var element_bottom_position = (element_top_position + element_height);
+
+            if ((element_bottom_position >= window_top_position) &&
+                (element_top_position <= window_bottom_position)) {
+                $element.addClass('in-view');
+
+            }
+        });
+    }
+    $window.on('scroll resize', check_if_in_view);
+    $window.trigger('scroll');
 });
