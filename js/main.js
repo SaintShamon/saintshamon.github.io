bfc0922859c434e884dbb95589b3d889a618db7d
Maxim <54853308+SaintShamon@users.noreply.github.com>
2021-08-04 10:29:27 +0300

    +

--- js/main.js
+++ js/main.js
@@ -1,9 +1,26 @@
-// check webp
-Modernizr.on('webp', function (result) {
-    if (result) {
-        // supported
-    } else {
-        // not-supported
-    }
-});
+$(document).ready(function () {
+    $('.search_btn').on('click', function () {
+        $('header .top_header .input_block').slideToggle(500);
+    });
 
+    $('.briefcase .btn').on('click', function () {
+        $('header .top_header .briefcase_block').slideToggle(1000);
+        $('header .top_header .main_block .center_block .our_curses a').toggleClass('clicked');
+    });
+
+    $('.courses_block').slick({
+        infinite: true,
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        dots: false,
+    });
+
+    $(".item").click(function () {
+        $(".gallery-wrap .item").removeClass("active");
+        $(this).addClass("active");
+    });
+
+    $('.cooperation_section .we_are_in_block .text').on('click', function () {
+        $('.cooperation_section .we_are_in_block').toggleClass('active');
+    });
+});
