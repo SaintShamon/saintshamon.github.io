4015e3350297f818303c712e109b6a53d05bbf2f
Maxim <54853308+SaintShamon@users.noreply.github.com>
2021-08-11 02:48:39 +0300

    +

--- js/main.js
+++ js/main.js
@@ -8,6 +8,14 @@ $(document).ready(function () {
         $('header .top_header .main_block .center_block .our_curses a').toggleClass('clicked');
     });
 
+    $('.menu_icon').on('click', function () {
+        $('header .top_header .main_menu_block').slideToggle(1000, function () {
+            if ($(this).is(':visible'))
+                $(this).css('display', 'flex');
+        });
+        $('header .top_header .main_block .right_block .menu_icon .icon_container').toggleClass('open_menu');
+    });
+
     $('.courses_block').slick({
         infinite: true,
         slidesToShow: 4,
@@ -20,7 +28,42 @@ $(document).ready(function () {
         $(this).addClass("active");
     });
 
-    $('.cooperation_section .we_are_in_block .text').on('click', function () {
-        $('.cooperation_section .we_are_in_block').toggleClass('active');
+    $('.we_are_in_block').mouseover(function () {
+        $('.cooperation_section .main_block').addClass('right');
     });
+
+    $('.we_are_in_block').mouseout(function () {
+        $('.cooperation_section .main_block').removeClass('right');
+    });
+
+    // Check active classes
+    var checkClass = function () {
+        if ($('.list_block').hasClass('show')) {
+            $('.list_block').removeClass('show');
+        }
+    };
+
+    // Category filters
+    $('.list_menu li.articles').click(function () {
+        checkClass();
+        $('.content_block .list_block.articles').toggleClass('show');
+    });
+    $('.list_menu li.videos').click(function () {
+        checkClass();
+        $('.content_block .list_block.videos').toggleClass('show');
+    });
+    $('.list_menu li.podcasts').click(function () {
+        checkClass();
+        $('.list_block.podcasts').toggleClass('show');
+    });
+    $('.list_menu li.books').click(function () {
+        checkClass();
+        $('.list_block.books').toggleClass('show');
+    });
+
+    // Active tag
+    $('.list_menu li').click(function () {
+        $('.list_menu li').removeClass('active');
+        $(this).addClass('active');
+    })
 });
