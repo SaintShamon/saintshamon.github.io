3ba3a13731345ef66af933d15f7c1df7e77085ea
Maxim <54853308+SaintShamon@users.noreply.github.com>
2022-04-05 23:55:27 +0300

    +

---
+++ js/slider.js
@@ -0,0 +1,23 @@
+var mySwiper = new Swiper('.swiper-container', {
+    speed: 400,
+    spaceBetween: 30,
+    a11y: true,
+    keyboardControl: true,
+    grabCursor: true,
+    slidesPerView: 1,
+    centeredSlides: true,
+    breakpoints: {
+        1201: {
+            slidesPerView: 2,
+            spaceBetween: 0,
+            a11y: true,
+            keyboardControl: true,
+            grabCursor: true,
+            centeredSlides: false,
+        },
+    },
+    navigation: {
+        nextEl: ".swiper-button-next-unique",
+        prevEl: ".swiper-button-prev-unique",
+    }
+});
