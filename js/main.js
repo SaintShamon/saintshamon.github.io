2b5ea3ed99541253f551080c5078821bd627f450
Maxim <54853308+SaintShamon@users.noreply.github.com>
2022-04-20 04:54:03 +0300

    new site

--- js/main.js
+++ js/main.js
@@ -1,9 +1,3 @@
-// Проблема длины при ховере
-
-$('.header_desctop nav li').each(function () {
-    $(this).width($(this).width() + 6);
-});
-
 // Ховер блока на главной
 
 $('.burger-container').click(function () {
@@ -13,22 +7,48 @@ $('.burger-container').click(function () {
     return false;
 });
 
-$(document).ready(function () {
-    $(".controls .swiper-slide").click(function () {
-        if ($('.page').hasClass('active-page')) {
-            $(".active-page").removeClass("active-page");
-        }
-        var pageIndex = $(this).index();
-        $next = $(".pages .page").eq(pageIndex);
+let wrapper = document.querySelector('.line'),
+    marquee = document.querySelector('.line span'),
+    wrapperWidth = wrapper.offsetWidth,
+    marqueeWidth = marquee.scrollWidth;
 
-        $(".active-control").removeClass("active-control");
-        $(this).addClass("active-control");
-        $next.addClass("active-page");
-    })
-})
+function move() {
+    var currentTX = getComputedStyle(marquee).transform.split(',');
+    if (currentTX[4] === undefined) {
+        currentTX = -1;
+    } else {
+        currentTX = parseFloat(currentTX[4]) - 1;
+    }
 
-$('.service_block li').click(function () {
-    $(this).parents().eq(1).find('li').removeClass('active');
-    $(this).toggleClass('active');
-    $(this).find('li').slideToggle(300);
-});
+    if (-currentTX >= marqueeWidth) {
+        marquee.style.transform = 'translateX(' + wrapperWidth + 'px)';
+
+    } else {
+        marquee.style.transform = 'translateX(' + currentTX + 'px)';
+    }
+}
+
+var interval = setInterval(move, 10);
+
+let wrapper2 = document.querySelector('.line_2'),
+    marquee2 = document.querySelector('.line_2 span'),
+    wrapperWidth2 = wrapper2.offsetWidth,
+    marqueeWidth2 = marquee2.scrollWidth;
+
+function move2() {
+    var currentTX2 = getComputedStyle(marquee2).transform.split(',');
+    if (currentTX2[4] === undefined) {
+        currentTX2 = -1;
+    } else {
+        currentTX2 = parseFloat(currentTX2[4]) - 1;
+    }
+
+    if (-currentTX2 >= marqueeWidth2) {
+        marquee2.style.transform = 'translateX(' + wrapperWidth2 + 'px)';
+
+    } else {
+        marquee2.style.transform = 'translateX(' + currentTX2 + 'px)';
+    }
+}
+
+var interval2 = setInterval(move2, 10);
