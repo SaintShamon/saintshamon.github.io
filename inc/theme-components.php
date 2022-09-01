0e9905daed756b526953c8d92de48182de9262e4
Maksym <maxkoytsan@gmail.com>
2022-09-01 12:01:45 +0300

    +

--- inc/theme-components.php
+++
@@ -1,5 +0,0 @@
-<?php
-/**
- * Remove Contact Form 7 auto added p tags
- */
-add_filter('wpcf7_autop_or_not', '__return_false');
--- index.php
+++
@@ -1,3 +0,0 @@
-<?php get_header();?>
-<?php require get_template_directory().'/index.html'; ?>
-<?php get_footer();?>
