8be9675f594249a9f50b36d56c8a7b2aa0cd2bfc
Maksym <maxkoytsan@gmail.com>
2022-10-14 12:39:06 +0300

    +

--- js/template-parts/blocks/slider.js
+++ js/template-parts/blocks/slider.js
@@ -11,6 +11,35 @@ function mainslider() {
             infinite: true,
             cssEase: 'linear',
             variableWidth: true,
+            responsive: [{
+                    breakpoint: 1400,
+                    settings: {
+                        slidesToShow: 3,
+                        variableWidth: false,
+                        centerMode: true,
+                    }
+                },
+                {
+                    breakpoint: 1240,
+                    settings: {
+                        slidesToShow: 2,
+                        slidesToScroll: 1
+                    }
+                },
+                {
+                    breakpoint: 994,
+                    settings: {
+                        slidesToShow: 1,
+                        slidesToScroll: 1,
+                        dots: true,
+                        centerMode: false,
+                        variableWidth: false,
+                    }
+                }
+                // You can unslick at a given breakpoint now by adding:
+                // settings: "unslick"
+                // instead of a settings object
+            ]
         });
     });
 
