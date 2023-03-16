926a21755622592aebf6fc33dfc8f3be14b71fff
Maksym Koitsan <maxkoytsan@gmail.com>
2023-03-16 14:28:49 +0200

    +

--- template-parts/loop/acf-blocks-loop.php
+++
@@ -1,9 +0,0 @@
-<?php
-if( have_rows('content_blocks') ):
-    while ( have_rows('content_blocks') ) : the_row();
-        get_template_part('template-parts/acf-blocks/'.get_row_layout());
-    endwhile;
-
-else :
-
-endif;
